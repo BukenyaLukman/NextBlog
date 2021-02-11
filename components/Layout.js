4ef625c4c1367d6bc4162af14a255793a9dd4b3f
BukenyaLukman <bukenyalukman@gmail.com>
2021-02-11 15:38:09 +0300

    Project Setup and Walkthrough of next

---
+++ components/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from '../styles/Layout.module.css';
+import Nav from './Nav';
+
+export const Layout = ({children}) => {
+    return (
+        <>
+        <Nav/>
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <h1>Hello</h1>
+                {children}
+            </main>  
+        </div>
+        </>
+    )
+}
+export default Layout;
